2cdd0123fcae5ede1b45cc6a98f52a8565769c4d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 23:02:57 +0800

    Add parent container

---
+++ Outapatient Management CRUD/Home.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelContainer = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'PanelContainer
+        '
+        Me.PanelContainer.Location = New System.Drawing.Point(249, 23)
+        Me.PanelContainer.Name = "PanelContainer"
+        Me.PanelContainer.Size = New System.Drawing.Size(1040, 715)
+        Me.PanelContainer.TabIndex = 0
+        '
+        'Home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1359, 750)
+        Me.Controls.Add(Me.PanelContainer)
+        Me.Name = "Home"
+        Me.Text = "Outpatient Management System"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PanelContainer As System.Windows.Forms.Panel
+End Class
