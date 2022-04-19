438babe626880a23bf9032315c574fef3acb2689
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 22:15:22 +0800

    Rename patient form

---
+++ Outapatient Management CRUD/Patients.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Patients
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PatientDataGrid = New System.Windows.Forms.DataGridView()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.AddBtn = New System.Windows.Forms.Button()
+        Me.SearchBtn = New System.Windows.Forms.Button()
+        Me.SearchBox = New System.Windows.Forms.TextBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FirstnameBox = New System.Windows.Forms.TextBox()
+        Me.LastnameBox = New System.Windows.Forms.TextBox()
+        Me.DOBPicker = New System.Windows.Forms.DateTimePicker()
+        Me.MaleRadio = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.FemaleRadio = New System.Windows.Forms.RadioButton()
+        Me.ContactBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PatientDataGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1017, 43)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(3, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(347, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Outpatient Management System"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Location = New System.Drawing.Point(978, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 22)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PatientDataGrid
+        '
+        Me.PatientDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.PatientDataGrid.BackgroundColor = System.Drawing.SystemColors.ControlLight
+        Me.PatientDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.PatientDataGrid.Location = New System.Drawing.Point(57, 53)
+        Me.PatientDataGrid.Name = "PatientDataGrid"
+        Me.PatientDataGrid.Size = New System.Drawing.Size(928, 237)
+        Me.PatientDataGrid.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.PatientDataGrid)
+        Me.Panel2.Location = New System.Drawing.Point(0, 40)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1014, 308)
+        Me.Panel2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(54, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Patients"
+        '
+        'AddBtn
+        '
+        Me.AddBtn.BackColor = System.Drawing.Color.SteelBlue
+        Me.AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AddBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddBtn.ForeColor = System.Drawing.Color.White
+        Me.AddBtn.Location = New System.Drawing.Point(161, 616)
+        Me.AddBtn.Name = "AddBtn"
+        Me.AddBtn.Size = New System.Drawing.Size(84, 39)
+        Me.AddBtn.TabIndex = 3
+        Me.AddBtn.Text = "Add"
+        Me.AddBtn.UseVisualStyleBackColor = False
+        '
+        'SearchBtn
+        '
+        Me.SearchBtn.BackColor = System.Drawing.Color.Green
+        Me.SearchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SearchBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SearchBtn.ForeColor = System.Drawing.Color.White
+        Me.SearchBtn.Location = New System.Drawing.Point(836, 363)
+        Me.SearchBtn.Name = "SearchBtn"
+        Me.SearchBtn.Size = New System.Drawing.Size(76, 39)
+        Me.SearchBtn.TabIndex = 4
+        Me.SearchBtn.Text = "Search"
+        Me.SearchBtn.UseVisualStyleBackColor = False
+        '
+        'SearchBox
+        '
+        Me.SearchBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SearchBox.Location = New System.Drawing.Point(34, 1)
+        Me.SearchBox.Name = "SearchBox"
+        Me.SearchBox.Size = New System.Drawing.Size(131, 35)
+        Me.SearchBox.TabIndex = 5
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Controls.Add(Me.SearchBox)
+        Me.Panel3.Location = New System.Drawing.Point(662, 363)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(168, 39)
+        Me.Panel3.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "ID"
+        '
+        'FirstnameBox
+        '
+        Me.FirstnameBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FirstnameBox.Location = New System.Drawing.Point(161, 368)
+        Me.FirstnameBox.Name = "FirstnameBox"
+        Me.FirstnameBox.Size = New System.Drawing.Size(217, 31)
+        Me.FirstnameBox.TabIndex = 7
+        '
+        'LastnameBox
+        '
+        Me.LastnameBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LastnameBox.Location = New System.Drawing.Point(161, 417)
+        Me.LastnameBox.Name = "LastnameBox"
+        Me.LastnameBox.Size = New System.Drawing.Size(217, 31)
+        Me.LastnameBox.TabIndex = 8
+        '
+        'DOBPicker
+        '
+        Me.DOBPicker.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DOBPicker.Location = New System.Drawing.Point(161, 468)
+        Me.DOBPicker.Name = "DOBPicker"
+        Me.DOBPicker.Size = New System.Drawing.Size(217, 20)
+        Me.DOBPicker.TabIndex = 9
+        '
+        'MaleRadio
+        '
+        Me.MaleRadio.AutoSize = True
+        Me.MaleRadio.Location = New System.Drawing.Point(16, 19)
+        Me.MaleRadio.Name = "MaleRadio"
+        Me.MaleRadio.Size = New System.Drawing.Size(48, 17)
+        Me.MaleRadio.TabIndex = 10
+        Me.MaleRadio.TabStop = True
+        Me.MaleRadio.Text = "Male"
+        Me.MaleRadio.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.FemaleRadio)
+        Me.GroupBox1.Controls.Add(Me.MaleRadio)
+        Me.GroupBox1.Location = New System.Drawing.Point(161, 503)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(188, 46)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sex"
+        '
+        'FemaleRadio
+        '
+        Me.FemaleRadio.AutoSize = True
+        Me.FemaleRadio.Location = New System.Drawing.Point(103, 19)
+        Me.FemaleRadio.Name = "FemaleRadio"
+        Me.FemaleRadio.Size = New System.Drawing.Size(59, 17)
+        Me.FemaleRadio.TabIndex = 11
+        Me.FemaleRadio.TabStop = True
+        Me.FemaleRadio.Text = "Female"
+        Me.FemaleRadio.UseVisualStyleBackColor = True
+        '
+        'ContactBox
+        '
+        Me.ContactBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ContactBox.Location = New System.Drawing.Point(161, 565)
+        Me.ContactBox.Name = "ContactBox"
+        Me.ContactBox.Size = New System.Drawing.Size(217, 31)
+        Me.ContactBox.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(52, 368)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 25)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Firstname"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(52, 420)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 25)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Lastname"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(53, 463)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(98, 25)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Birthdate"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(52, 514)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 25)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Sex"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(52, 565)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(86, 25)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Contact"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Brown
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(294, 616)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 39)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1017, 663)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ContactBox)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DOBPicker)
+        Me.Controls.Add(Me.LastnameBox)
+        Me.Controls.Add(Me.FirstnameBox)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.SearchBtn)
+        Me.Controls.Add(Me.AddBtn)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "t"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PatientDataGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PatientDataGrid As System.Windows.Forms.DataGridView
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents AddBtn As System.Windows.Forms.Button
+    Friend WithEvents SearchBtn As System.Windows.Forms.Button
+    Friend WithEvents SearchBox As System.Windows.Forms.TextBox
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents FirstnameBox As System.Windows.Forms.TextBox
+    Friend WithEvents LastnameBox As System.Windows.Forms.TextBox
+    Friend WithEvents DOBPicker As System.Windows.Forms.DateTimePicker
+    Friend WithEvents MaleRadio As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents FemaleRadio As System.Windows.Forms.RadioButton
+    Friend WithEvents ContactBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
