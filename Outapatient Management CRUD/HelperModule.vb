0372b51f81fa63becefbc32e58faa6893949957c
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 22:15:03 +0800

    Add helper module

---
+++ Outapatient Management CRUD/HelperModule.vb
@@ -0,0 +1,9 @@
+﻿Module HelperModule
+    Public Function IsInputNumeric(ByVal input As String) As Boolean
+        If String.IsNullOrWhiteSpace(input) Then Return False
+        If IsNumeric(input) Then Return True
+        Dim parts() As String = input.Split("/"c)
+        If parts.Length <> 2 Then Return False
+        Return IsNumeric(parts(0)) AndAlso IsNumeric(parts(1))
+    End Function
+End Module
