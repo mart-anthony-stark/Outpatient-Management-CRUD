6829da95a777ceac41d2b1f24acf2bacce11960b
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 23:16:16 +0800

    Add sidebar

--- Outapatient Management CRUD/Home.Designer.vb
+++ Outapatient Management CRUD/Home.Designer.vb
@@ -23,26 +23,118 @@ Partial Class Home
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PanelContainer = New System.Windows.Forms.Panel()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.MinimizeBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.PanelContainer.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'PanelContainer
         '
-        Me.PanelContainer.Location = New System.Drawing.Point(249, 23)
+        Me.PanelContainer.Controls.Add(Me.ToolStrip1)
+        Me.PanelContainer.Location = New System.Drawing.Point(238, 57)
         Me.PanelContainer.Name = "PanelContainer"
         Me.PanelContainer.Size = New System.Drawing.Size(1040, 715)
         Me.PanelContainer.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.MinimizeBtn)
+        Me.Panel1.Controls.Add(Me.ExitBtn)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1359, 51)
+        Me.Panel1.TabIndex = 1
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.BackColor = System.Drawing.Color.MediumVioletRed
+        Me.ExitBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ExitBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitBtn.ForeColor = System.Drawing.Color.White
+        Me.ExitBtn.Location = New System.Drawing.Point(1304, 8)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(43, 30)
+        Me.ExitBtn.TabIndex = 0
+        Me.ExitBtn.Text = "X"
+        Me.ExitBtn.UseVisualStyleBackColor = False
+        '
+        'MinimizeBtn
+        '
+        Me.MinimizeBtn.BackColor = System.Drawing.Color.MediumVioletRed
+        Me.MinimizeBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MinimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MinimizeBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MinimizeBtn.ForeColor = System.Drawing.Color.White
+        Me.MinimizeBtn.Location = New System.Drawing.Point(1238, 8)
+        Me.MinimizeBtn.Name = "MinimizeBtn"
+        Me.MinimizeBtn.Size = New System.Drawing.Size(43, 30)
+        Me.MinimizeBtn.TabIndex = 1
+        Me.MinimizeBtn.Text = "--"
+        Me.MinimizeBtn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(347, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Outpatient Management System"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel2.Location = New System.Drawing.Point(0, 51)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(232, 733)
+        Me.Panel2.TabIndex = 2
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1040, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
         '
         'Home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1359, 750)
+        Me.ClientSize = New System.Drawing.Size(1359, 784)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.PanelContainer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Home"
         Me.Text = "Outpatient Management System"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.PanelContainer.ResumeLayout(False)
+        Me.PanelContainer.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents PanelContainer As System.Windows.Forms.Panel
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents MinimizeBtn As System.Windows.Forms.Button
+    Friend WithEvents ExitBtn As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
 End Class
