c94a80e83e9ecbf13b99fdce54551a4db856bdb2
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 23:52:09 +0800

    Add doctors panel

--- Outapatient Management CRUD/Home.vb
+++ Outapatient Management CRUD/Home.vb
@@ -1,10 +1,8 @@
 ﻿Public Class Home
+    Dim activePanel As String
 
     Private Sub Home_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Patients.TopLevel = False
-        Patients.TopMost = True
-        Me.PanelContainer.Controls.Add(Patients)
-        Patients.Show()
+
     End Sub
 
     Private Sub ExitBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitBtn.Click
@@ -13,5 +11,27 @@
 
     Private Sub MinimizeBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MinimizeBtn.Click
         WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub LoadForm(ByVal form As Form)
+        Me.PanelContainer.Controls.Clear()
+        activePanel = form.Name
+        RefreshBtnsStyles()
+        form.TopLevel = False
+        form.TopMost = True
+        Me.PanelContainer.Controls.Add(form)
+        form.Show()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PatentsBtn.Click
+        LoadForm(Patients)
+    End Sub
+
+    Private Sub DoctorsBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DoctorsBtn.Click
+        LoadForm(Doctors)
+    End Sub
+
+    Private Sub RefreshBtnsStyles()
+        MsgBox(activePanel)
     End Sub
 End Class
