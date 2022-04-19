37ddec3d343463ef665e6b64a7470c9a5c72744d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-19 23:03:02 +0800

    Add parent container

---
+++ Outapatient Management CRUD/Home.vb
@@ -0,0 +1,9 @@
+﻿Public Class Home
+
+    Private Sub Home_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Patients.TopLevel = False
+        Patients.TopMost = True
+        Me.PanelContainer.Controls.Add(Patients)
+        Patients.Show()
+    End Sub
+End Class
