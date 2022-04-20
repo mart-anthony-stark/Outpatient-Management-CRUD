5b5fa1ad69a9dcfdc3206ff2dd15efe8247b782a
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-20 19:39:19 +0800

    Add delete functionality

--- Outapatient Management CRUD/Patients.vb
+++ Outapatient Management CRUD/Patients.vb
@@ -2,11 +2,7 @@
     Private currentPatId As String
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Try
-            reload("SELECT patient_id AS ID, concat(firstname, ' ',lastname) AS Name, date_of_birth AS 'Date of Birth', sex AS Sex, contact_number AS Contact FROM patient", PatientDataGrid)
-        Catch ex As Exception
-            MsgBox(ex.Message)
-        End Try
+            FetchAllPatients()
     End Sub
 
     'Handles add patient'
@@ -27,7 +23,7 @@
             Try
                 readuery("INSERT INTO patient VALUES(null, '" & firstname & "', '" & lastname & "', '" & dob & "', '" & sex & "', '" & contact & "')")
                 MsgBox("Patient was successfully added to database")
-                reload("SELECT patient_id AS ID, concat(firstname, ' ',lastname) AS Name, date_of_birth AS 'Date of Birth', sex AS Sex, contact_number AS Contact FROM patient", PatientDataGrid)
+                FetchAllPatients()
                 FirstnameBox.Text = ""
                 LastnameBox.Text = ""
                 MaleRadio.Checked = False
@@ -75,6 +71,7 @@
         strconn.Close()
     End Sub
 
+    'Clears add fields'
     Private Sub CancelAddBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CancelAddBtn.Click
         FirstnameBox.Text = ""
         LastnameBox.Text = ""
@@ -85,6 +82,7 @@
         ToggleBtns()
     End Sub
 
+    'Toggles edit buttons visbility (sav, delete, cancel) '
     Private Sub ToggleBtns()
         Dim state As Boolean = Not String.IsNullOrEmpty(currentPatId)
         EditCancelBtn.Visible = state
@@ -96,6 +94,7 @@
         ClearEdit()
     End Sub
 
+    'Removes current patient id to be edited; Clears edit fields; Hides edit buttons'
     Private Sub ClearEdit()
         currentPatId = Nothing
         EditFname.Text = ""
@@ -108,6 +107,7 @@
         ToggleBtns()
     End Sub
 
+    'Handles updating data for specific patient row'
     Private Sub EditSaveBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EditSaveBtn.Click
         Dim fname, lname, dob, sex, contact As String
         fname = EditFname.Text
@@ -120,16 +120,41 @@
         End If
         contact = EditContact.Text
         If (String.IsNullOrEmpty(fname) Or String.IsNullOrEmpty(lname) Or String.IsNullOrEmpty(dob) Or String.IsNullOrEmpty(sex) Or String.IsNullOrEmpty(contact)) Then
-            MsgBox("You must fill all input fields to add")
+            MsgBox("You must fill all input fields to save record")
         Else
             Try
                 readuery("UPDATE patient SET firstname='" & fname & "', lastname='" & lname & "', date_of_birth='" & dob & "', sex='" & sex & "', contact_number='" & contact & "' WHERE patient_id=" & currentPatId & "")
                 MsgBox("Patient record was successfully updated")
-                reload("SELECT patient_id AS ID, concat(firstname, ' ',lastname) AS Name, date_of_birth AS 'Date of Birth', sex AS Sex, contact_number AS Contact FROM patient", PatientDataGrid)
+                FetchAllPatients()
                 ClearEdit()
             Catch ex As Exception
                 MsgBox(ex.Message)
             End Try
         End If
+    End Sub
+
+    Private Sub DeleteBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DeleteBtn.Click
+        Dim ans As String
+        ans = MsgBox("Confirm Update", vbYesNo)
+        If ans = vbYes Then
+            Try
+                readuery("DELETE FROM patient WHERE patient_id=" & currentPatId)
+                FetchAllPatients()
+                MsgBox("Patient record was successfully deleted")
+                ClearEdit()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    
+    End Sub
+
+    'Get all patient data in db'
+    Private Sub FetchAllPatients()
+        Try
+            reload("SELECT patient_id AS ID, concat(firstname, ' ',lastname) AS Name, date_of_birth AS 'Date of Birth', sex AS Sex, contact_number AS Contact FROM patient", PatientDataGrid)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 End Class
