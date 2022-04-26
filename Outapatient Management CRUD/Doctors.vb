959dc7b383343ff75688a49a34ce754a170bc895
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-26 14:24:47 +0800

    Add doctors UI

--- Outapatient Management CRUD/Doctors.vb
+++ Outapatient Management CRUD/Doctors.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Doctors
 
+    Private Sub Doctors_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FetchData()
+    End Sub
+
+    Private Sub FetchData()
+        Try
+            reload("SELECT doctor_id AS ID, concat(lastname, ', ', firstname) AS Name, specialization AS Specialization, contact_number AS 'Contact Number' from doctor", DoctorDataGridView)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
 End Class
