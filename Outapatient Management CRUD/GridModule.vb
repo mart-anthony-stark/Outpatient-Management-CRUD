f9bd6001350805f0b94d5df36781eefd573590db
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-27 20:29:21 +0800

    Add: datagridcontrols module

---
+++ Outapatient Management CRUD/GridModule.vb
@@ -0,0 +1,22 @@
+﻿Module GridModule
+    Public Sub AddControls(ByVal DTG)
+        Dim editBtn, deleteBtn As New DataGridViewButtonColumn
+        editBtn.HeaderText = "Edit"
+        editBtn.Text = "Edit"
+        editBtn.Name = "edit"
+        editBtn.UseColumnTextForButtonValue = True
+        editBtn.FlatStyle = FlatStyle.Flat
+        editBtn.DefaultCellStyle.BackColor = Color.MediumSeaGreen
+        editBtn.DefaultCellStyle.ForeColor = Color.White
+        DTG.Columns.Insert(DTG.Columns.Count, editBtn)
+
+        deleteBtn.HeaderText = "Delete"
+        deleteBtn.Text = "Delete"
+        deleteBtn.Name = "Delete"
+        deleteBtn.UseColumnTextForButtonValue = True
+        deleteBtn.FlatStyle = FlatStyle.Flat
+        deleteBtn.DefaultCellStyle.BackColor = Color.Brown
+        deleteBtn.DefaultCellStyle.ForeColor = Color.White
+        DTG.Columns.Insert(DTG.Columns.Count, deleteBtn)
+    End Sub
+End Module
