4baaa0bc7b798a6b9d3d9576c5c9bf1db3f6d7a0
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-09 13:36:32 +0800

    Add: login UI

---
+++ Outapatient Management CRUD/Login.vb
@@ -0,0 +1,27 @@
+﻿Public Class Login
+
+    Private Sub Login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim email, password As String
+        email = EmailBox.Text
+        password = PasswordBox.Text
+        If (String.IsNullOrEmpty(email) Or String.IsNullOrEmpty(password)) Then
+            MessageBox.Show("Please enter your email and password", "Empty fields", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MsgBox("Logged in")
+            Me.Hide()
+            Main.Show()
+        End If
+    End Sub
+End Class
