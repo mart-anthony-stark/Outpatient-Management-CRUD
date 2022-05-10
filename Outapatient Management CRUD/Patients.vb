fd58c8864938c5eeec06143a370ec01180e09d89
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-10 13:37:48 +0800

    Add: laboratory section UI

--- Outapatient Management CRUD/Patients.vb
+++ Outapatient Management CRUD/Patients.vb
@@ -185,4 +185,8 @@
         End If
 
     End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
