731a36c23e9af6918780e74630907fa59e2b3095
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-11 21:49:26 +0800

    Add: insert & delete appointment

--- Outapatient Management CRUD/Appointments.vb
+++ Outapatient Management CRUD/Appointments.vb
@@ -6,6 +6,12 @@ Public Class Appointments
     Private Sub Appointments_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         FetchCmbData()
         GetCurrentTime()
+        FetchAppointments()
+        AddControls(AppointmentTbl)
+    End Sub
+
+    Private Sub FetchAppointments()
+        reload("SELECT apt_id AS ID, concat(patient.lastname,', ', patient.firstname) AS Patient, concat(doctor.lastname,', ', doctor.firstname) AS Doctor, date AS Date, time AS Time, apt_type AS Type FROM appointment, patient, doctor", AppointmentTbl)
     End Sub
 
     Public Sub FetchCmbData()
@@ -48,9 +54,76 @@ Public Class Appointments
     End Sub
 
     'Clear add inputs
-    Private Sub ClearAddBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearAddBtn.Click
+    Private Sub ClearAddInputs()
         PatientCmbBox.SelectedItem = Nothing
         DoctorsCmbBox.SelectedItem = Nothing
         AddAptTypeInput.Text = ""
+    End Sub
+
+    Private Sub ClearAddBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearAddBtn.Click
+        ClearAddInputs()
+    End Sub
+
+    'Add new appointment record
+    Private Sub AddBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AddBtn.Click
+        Dim patient, doctor, time, apt_type As String
+        Dim hrs As Integer = Hours.Value
+
+        If (ampm.Text.Equals("PM") And Hours.Value < 12) Then
+            hrs = Hours.Value + 12
+        ElseIf (ampm.Text.Equals("AM") And Hours.Value = 12) Then
+            hrs = Hours.Value - 12
+        End If
+        time = String.Format("{0:00}:{1:00}", CInt(Math.Floor(hrs)), Minutes.Value)
+        Dim apt_date As String = Format(DatePicker.Value, "yyyy-MM-dd")
+        apt_type = AddAptTypeInput.Text
+
+        If (DoctorsCmbBox.SelectedIndex < 0 Or PatientCmbBox.SelectedIndex < 0 Or String.IsNullOrEmpty(time) Or String.IsNullOrEmpty(apt_date) Or String.IsNullOrEmpty(apt_type)) Then
+            MessageBox.Show("Input fields must be filled", "Invalid Inputs", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            patient = PatientCmbBox.SelectedValue.ToString
+            doctor = DoctorsCmbBox.SelectedValue.ToString
+            Try
+                readuery("INSERT INTO appointment VALUES(null, '" & patient & "', '" & doctor & "', '" & apt_date & "', '" & time & "', '" & apt_type & "')")
+                MsgBox("Appointment was successfully added to database")
+                ClearAddInputs()
+                FetchAppointments()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub DeleteAppointment(ByVal id As String)
+        Dim ans As String
+        ans = MsgBox("Are you sure you wamt to delete this record?", vbYesNo)
+        If ans = vbYes Then
+            Try
+                readuery("DELETE FROM appointment WHERE apt_id=" & id)
+                FetchAppointments()
+                MsgBox("Appointment record was successfully deleted")
+                'ClearEdit()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    'Handle Inline edit and delete button
+    Private Sub AppointmentTbl_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles AppointmentTbl.CellContentClick
+        Dim senderGrid = DirectCast(sender, DataGridView)
+        Dim column As System.Windows.Forms.DataGridViewColumn = senderGrid.Columns(e.ColumnIndex)
+        Dim row As System.Windows.Forms.DataGridViewRow = AppointmentTbl.Rows(e.RowIndex)
+        Dim id As String = row.Cells(2).Value.ToString
+        'MsgBox(id & " - " & column.HeaderText)
+        If TypeOf column Is DataGridViewButtonColumn AndAlso
+            e.RowIndex >= 0 Then
+            'TODO - Button Clicked - Execute Code Here
+            If (column.HeaderText Is "Edit") Then
+                'SearchData(id)
+            ElseIf (column.HeaderText Is "Delete") Then
+                DeleteAppointment(id)
+            End If
+        End If
     End Sub
 End Class
