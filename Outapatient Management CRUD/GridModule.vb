9b7f8820fc9abda00a8b7436914f1cc9645e7df2
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-12 20:39:22 +0800

    Add: edit button to datagrid helper module

--- Outapatient Management CRUD/GridModule.vb
+++ Outapatient Management CRUD/GridModule.vb
@@ -19,4 +19,16 @@
         deleteBtn.DefaultCellStyle.ForeColor = Color.White
         DTG.Columns.Insert(DTG.Columns.Count, deleteBtn)
     End Sub
+
+    Public Sub AddEdit(ByVal DTG)
+        Dim editBtn, deleteBtn As New DataGridViewButtonColumn
+        editBtn.HeaderText = "Edit"
+        editBtn.Text = "Edit"
+        editBtn.Name = "edit"
+        editBtn.UseColumnTextForButtonValue = True
+        editBtn.FlatStyle = FlatStyle.Flat
+        editBtn.DefaultCellStyle.BackColor = Color.MediumSeaGreen
+        editBtn.DefaultCellStyle.ForeColor = Color.White
+        DTG.Columns.Insert(DTG.Columns.Count, editBtn)
+    End Sub
 End Module
