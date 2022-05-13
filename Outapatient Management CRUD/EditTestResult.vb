08bf5b4948b8113b0e0e0207076cc9aea1fa8034
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-13 21:39:05 +0800

    Add: fetch labtests

--- Outapatient Management CRUD/EditTestResult.vb
+++ Outapatient Management CRUD/EditTestResult.vb
@@ -1,4 +1,6 @@
-﻿Public Class EditTestResult
+﻿Imports MySql.Data.MySqlClient
+
+Public Class EditTestResult
 
     Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
         WindowState = FormWindowState.Minimized
@@ -10,5 +12,30 @@
 
     Private Sub EditTestResult_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.CenterToScreen()
+        FetchLabtests()
+    End Sub
+
+    'Handle retrieve labtest result
+    Public Sub FetchLabtests()
+        Try
+            DiseaseCmb.SelectedText = Nothing
+            LabtestCmb.SelectedText = Nothing
+            Dim test_table As New DataTable()
+            Dim test_adapter As New MySqlDataAdapter("SELECT test_id, concat(test_name,' (',test_type, ')') AS display from laboratorytest", strconn)
+            Dim disease_table As New DataTable()
+            Dim disease_adapter As New MySqlDataAdapter("SELECT disease_id, concat(name,' (',type, ')') AS display from disease", strconn)
+
+            test_adapter.Fill(test_table)
+            LabtestCmb.DataSource = test_table
+            LabtestCmb.ValueMember = "test_id"
+            LabtestCmb.DisplayMember = "display"
+
+            disease_adapter.Fill(disease_table)
+            DiseaseCmb.DataSource = disease_table
+            DiseaseCmb.ValueMember = "disease_id"
+            DiseaseCmb.DisplayMember = "display"
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 End Class
