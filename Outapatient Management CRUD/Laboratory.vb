c51dd571a34354bf1bf27650820f00995bd041f5
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-13 13:30:22 +0800

    Add: try catch

--- Outapatient Management CRUD/Laboratory.vb
+++ Outapatient Management CRUD/Laboratory.vb
@@ -25,20 +25,25 @@
 
     'Method that handles edit or delete click in disease table
     Private Sub DiseaseTbl_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DiseaseTbl.CellContentClick
-        Dim senderGrid = DirectCast(sender, DataGridView)
-        Dim column As System.Windows.Forms.DataGridViewColumn = senderGrid.Columns(e.ColumnIndex)
-        Dim row As System.Windows.Forms.DataGridViewRow = DiseaseTbl.Rows(e.RowIndex)
-        Dim id As String = row.Cells(2).Value.ToString
-        MsgBox(column.HeaderText)
-        If TypeOf column Is DataGridViewButtonColumn AndAlso
-            e.RowIndex >= 0 Then
-            'TODO - Button Clicked - Execute Code Here
-            If (column.HeaderText Is "Edit") Then
-                'SearchData(id)
-            ElseIf (column.HeaderText Is "Delete") Then
-                'DeleteDoctor(id)
+        Try
+            Dim senderGrid = DirectCast(sender, DataGridView)
+            Dim column As System.Windows.Forms.DataGridViewColumn = senderGrid.Columns(e.ColumnIndex)
+            Dim row As System.Windows.Forms.DataGridViewRow = DiseaseTbl.Rows(e.RowIndex)
+            Dim id As String = row.Cells(2).Value.ToString
+            MsgBox(column.HeaderText)
+            If TypeOf column Is DataGridViewButtonColumn AndAlso
+                e.RowIndex >= 0 Then
+                'TODO - Button Clicked - Execute Code Here
+                If (column.HeaderText Is "Edit") Then
+                    'SearchData(id)
+                ElseIf (column.HeaderText Is "Delete") Then
+                    'DeleteDoctor(id)
+                End If
             End If
-        End If
+        Catch ex As Exception
+
+        End Try
+        
     End Sub
 
     Private Sub LabtestTbl_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles LabtestTbl.CellContentClick
