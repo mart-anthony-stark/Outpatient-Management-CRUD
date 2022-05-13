654ce32549abc9296e1d885c3941a78316e1ef85
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-13 15:31:05 +0800

    Fixed: appointment retrieval

---
+++ Outapatient Management CRUD/EditTestResult.vb
@@ -0,0 +1,14 @@
+﻿Public Class EditTestResult
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub EditTestResult_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+End Class
