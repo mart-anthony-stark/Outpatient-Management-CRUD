0d9e1b5b20025fc868ce9a725828702ce17ba3d3
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-15 13:59:57 +0800

    Add: admin settings

---
+++ Outapatient Management CRUD/AdminSettings.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AdminSettings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabtestLbl = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.firstname = New System.Windows.Forms.Label()
+        Me.lastname = New System.Windows.Forms.Label()
+        Me.email = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.userID = New System.Windows.Forms.Label()
+        Me.ClearAddBtn = New System.Windows.Forms.Button()
+        Me.EditNameBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(238, 197)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 25)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Firstname"
+        '
+        'LabtestLbl
+        '
+        Me.LabtestLbl.AutoSize = True
+        Me.LabtestLbl.Font = New System.Drawing.Font("Arial Rounded MT Bold", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabtestLbl.Location = New System.Drawing.Point(29, 35)
+        Me.LabtestLbl.Name = "LabtestLbl"
+        Me.LabtestLbl.Size = New System.Drawing.Size(164, 24)
+        Me.LabtestLbl.TabIndex = 20
+        Me.LabtestLbl.Text = "Admin Settings"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(238, 264)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 25)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Lastname"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(238, 327)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 25)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Email"
+        '
+        'firstname
+        '
+        Me.firstname.AutoSize = True
+        Me.firstname.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.firstname.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.firstname.Location = New System.Drawing.Point(376, 197)
+        Me.firstname.Name = "firstname"
+        Me.firstname.Size = New System.Drawing.Size(107, 25)
+        Me.firstname.TabIndex = 23
+        Me.firstname.Text = "Firstname"
+        '
+        'lastname
+        '
+        Me.lastname.AutoSize = True
+        Me.lastname.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lastname.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.lastname.Location = New System.Drawing.Point(376, 264)
+        Me.lastname.Name = "lastname"
+        Me.lastname.Size = New System.Drawing.Size(107, 25)
+        Me.lastname.TabIndex = 24
+        Me.lastname.Text = "Firstname"
+        '
+        'email
+        '
+        Me.email.AutoSize = True
+        Me.email.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.email.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.email.Location = New System.Drawing.Point(376, 327)
+        Me.email.Name = "email"
+        Me.email.Size = New System.Drawing.Size(107, 25)
+        Me.email.TabIndex = 25
+        Me.email.Text = "Firstname"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(238, 140)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 25)
+        Me.Label4.TabIndex = 26
+        Me.Label4.Text = "User ID"
+        '
+        'userID
+        '
+        Me.userID.AutoSize = True
+        Me.userID.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.userID.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.userID.Location = New System.Drawing.Point(376, 140)
+        Me.userID.Name = "userID"
+        Me.userID.Size = New System.Drawing.Size(107, 25)
+        Me.userID.TabIndex = 27
+        Me.userID.Text = "Firstname"
+        '
+        'ClearAddBtn
+        '
+        Me.ClearAddBtn.BackColor = System.Drawing.Color.IndianRed
+        Me.ClearAddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ClearAddBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearAddBtn.ForeColor = System.Drawing.Color.White
+        Me.ClearAddBtn.Location = New System.Drawing.Point(575, 489)
+        Me.ClearAddBtn.Name = "ClearAddBtn"
+        Me.ClearAddBtn.Size = New System.Drawing.Size(215, 39)
+        Me.ClearAddBtn.TabIndex = 33
+        Me.ClearAddBtn.Text = "Delete Account"
+        Me.ClearAddBtn.UseVisualStyleBackColor = False
+        '
+        'EditNameBtn
+        '
+        Me.EditNameBtn.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.EditNameBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.EditNameBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EditNameBtn.ForeColor = System.Drawing.Color.White
+        Me.EditNameBtn.Location = New System.Drawing.Point(575, 383)
+        Me.EditNameBtn.Name = "EditNameBtn"
+        Me.EditNameBtn.Size = New System.Drawing.Size(215, 39)
+        Me.EditNameBtn.TabIndex = 32
+        Me.EditNameBtn.Text = "Edit Username"
+        Me.EditNameBtn.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(69, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(575, 428)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(215, 39)
+        Me.Button1.TabIndex = 34
+        Me.Button1.Text = "Change Password"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.carbon_email
+        Me.PictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox4.Location = New System.Drawing.Point(202, 322)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(30, 30)
+        Me.PictureBox4.TabIndex = 31
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.lname
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox3.Location = New System.Drawing.Point(202, 259)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(30, 30)
+        Me.PictureBox3.TabIndex = 30
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.fname
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(202, 192)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(30, 30)
+        Me.PictureBox2.TabIndex = 29
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.userID
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(202, 135)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(30, 30)
+        Me.PictureBox1.TabIndex = 28
+        Me.PictureBox1.TabStop = False
+        '
+        'AdminSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1121, 715)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ClearAddBtn)
+        Me.Controls.Add(Me.EditNameBtn)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.userID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.email)
+        Me.Controls.Add(Me.lastname)
+        Me.Controls.Add(Me.firstname)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LabtestLbl)
+        Me.Controls.Add(Me.Label2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "AdminSettings"
+        Me.Text = "SEttings"
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents LabtestLbl As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents firstname As System.Windows.Forms.Label
+    Friend WithEvents lastname As System.Windows.Forms.Label
+    Friend WithEvents email As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents userID As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents ClearAddBtn As System.Windows.Forms.Button
+    Friend WithEvents EditNameBtn As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
